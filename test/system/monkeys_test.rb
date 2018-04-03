46a61f4ef256d51a90ebe86720b8d41fcaeab375
mvizm2015 <mariaviz2015@gmail.com>
2018-04-02 20:54:03 -0400

    simple scaffold

---
+++ test/system/monkeys_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MonkeysTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit monkeys_url
+  #
+  #   assert_selector "h1", text: "Monkey"
+  # end
+end
